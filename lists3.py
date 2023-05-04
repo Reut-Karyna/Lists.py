956e3c78c5a8e72ecdb8dff0b7edea95f0a474d3
Reut Karyna <89328520+Reut-Karyna@users.noreply.github.com>
2023-05-05 04:28:26 +0800

    Update lists3.py

--- lists3.py
+++ lists3.py
@@ -1,6 +1,6 @@
 toppings =  ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
 
-prices = [2, 6, 1, 3, 2, 7, 2]
+prices = [2, 6, 1, 3, 2, 7, 2] 
 
 num_two_dollar_slices = prices.count(2)
 num_pizzas = len(toppings)
